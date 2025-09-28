bb8de7f3621d03ce38dd8867a806be57a00e58c8
Xifire <95431075+akashiyaki01c@users.noreply.github.com>
2025-09-29 01:45:50 +0900

    chore: eslint-plugin-unicorn, eslint-plugin-perfectionist を導入

--- eslint.config.js
+++ eslint.config.js
@@ -1,43 +1,49 @@
 import { FlatCompat } from '@eslint/eslintrc';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import importPlugin from 'eslint-plugin-import';
+import perfectionist from 'eslint-plugin-perfectionist';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
-import importPlugin from 'eslint-plugin-import';
-import tseslint from '@typescript-eslint/eslint-plugin';
+import unicorn from 'eslint-plugin-unicorn';
 
 const compat = new FlatCompat();
 
 export default [
-	{
-		ignores: ['dist/**', 'src-tauri/**'],
-	},
-	{
-		files: ['**/*.{js,jsx,ts,tsx}'],
-		languageOptions: {
-			ecmaVersion: 2020,
-			parser: '@typescript-eslint/parser',
-		},
-		plugins: {
-			'react-hooks': reactHooks,
-			'react-refresh': reactRefresh,
-			import: importPlugin,
-			'@typescript-eslint': tseslint,
-		},
-		settings: {
-			react: {
-				version: 'detect',
-			},
-			'import/resolver': {
-				typescript: {},
-			},
-		},
-		rules: {
-			...reactHooks.configs.recommended.rules,
-			...tseslint.configs.recommended.rules,
-		},
-	},
-	...compat.extends(
-		'plugin:@typescript-eslint/recommended',
-		'plugin:import/recommended',
-		'prettier'
-	),
+  {
+    ignores: ['dist/**', 'src-tauri/**'],
+  },
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      parser: '@typescript-eslint/parser',
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+      import: importPlugin,
+      perfectionist,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      unicorn,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      ...tseslint.configs.recommended.rules,
+      ...unicorn.configs.recommended.rules,
+      ...perfectionist.configs['recommended-natural-legacy'].rules,
+    },
+    settings: {
+      'import/resolver': {
+        typescript: {},
+      },
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+  ...compat.extends(
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'prettier',
+  ),
 ];
