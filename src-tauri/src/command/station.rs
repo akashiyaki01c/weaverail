2a41697a6d0a34e87cfc9e915cecbb8a438fbca9
Xifire <95431075+akashiyaki01c@users.noreply.github.com>
2026-04-15 21:12:43 +0900

    update: 不要なEmitを削除

--- src-tauri/src/command/station.rs
+++ src-tauri/src/command/station.rs
@@ -1,6 +1,5 @@
 use std::sync::Mutex;
 
-use tauri::{AppHandle, Emitter};
 use weaverail_model::{
     app::AppState,
     command::station::{AddStationCommand, RemoveStationCommand},
@@ -11,15 +10,12 @@ use weaverail_model::{
 pub async fn add_station(
     state: tauri::State<'_, Mutex<AppState>>,
     station: Station,
-    app: AppHandle,
 ) -> Result<(), String> {
     let command = AddStationCommand::new(station.clone());
     let mut state = state.lock().expect("mutex lock error");
 
     let command_manager = &mut state.command_manager;
     command_manager.execute(Box::new(command));
-
-    let _ = app.emit_filter("station_changed", &command_manager.root, |_| true);
 
     Ok(())
 }
@@ -28,15 +24,12 @@ pub async fn add_station(
 pub async fn remove_station(
     state: tauri::State<'_, Mutex<AppState>>,
     station_id: StationId,
-    app: AppHandle,
 ) -> Result<(), String> {
     let command = RemoveStationCommand::new(station_id);
     let mut state = state.lock().expect("mutex lock error");
 
     let command_manager = &mut state.command_manager;
     command_manager.execute(Box::new(command));
-
-    let _ = app.emit_filter("station_changed", &command_manager.root, |_| true);
 
     Ok(())
 }
