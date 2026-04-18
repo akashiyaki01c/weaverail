1ac3fa874dcb59f5c740df9d0d29d8ca0bea8708
Xifire <95431075+akashiyaki01c@users.noreply.github.com>
2026-04-19 00:07:15 +0900

    update: 可読性のためにバインディングのエクスポート順を変更

--- packages/types/index.ts
+++ packages/types/index.ts
@@ -1,30 +1,36 @@
 export * from "./bindings/DiagramRoot";
 export * from "./bindings/ExtensionProperty";
+export * from "./bindings/WeaverailId";
+
+export * from "./bindings/Station";
+export * from "./bindings/StationId";
+export * from "./bindings/Track";
+export * from "./bindings/TrackId";
+
 export * from "./bindings/Line";
 export * from "./bindings/LineId";
 export * from "./bindings/LineSegment";
 export * from "./bindings/LineSegmentId";
-export * from "./bindings/SegmentTrainOrder";
-export * from "./bindings/Station";
-export * from "./bindings/StationId";
-export * from "./bindings/StopType";
-export * from "./bindings/TemplateSegment";
+
+export * from "./bindings/TrainType";
+export * from "./bindings/TrainTypeId";
+
 export * from "./bindings/TemplateTrain";
 export * from "./bindings/TemplateTrainId";
 export * from "./bindings/TemplateTrainSegment";
 export * from "./bindings/TemplateTrainSegmentId";
 export * from "./bindings/TemplateTrainStation";
 export * from "./bindings/TemplateTrainStationId";
+export * from "./bindings/StopType";
 export * from "./bindings/Time";
+
 export * from "./bindings/Timetable";
 export * from "./bindings/TimetableId";
-export * from "./bindings/Track";
-export * from "./bindings/TrackId";
 export * from "./bindings/Train";
 export * from "./bindings/TrainId";
+export * from "./bindings/TemplateSegment";
+
+export * from "./bindings/SegmentTrainOrder";
 export * from "./bindings/TrainsAdjustment";
 export * from "./bindings/TrainsAdjustmentId";
 export * from "./bindings/TrainsAdjustmentType";
-export * from "./bindings/TrainType";
-export * from "./bindings/TrainTypeId";
-export * from "./bindings/WeaverailId";
